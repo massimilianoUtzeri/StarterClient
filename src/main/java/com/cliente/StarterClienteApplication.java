5f931a50a5f23543b1c45d6023b3e65dcd65b226
Massimiliano <utzerimassimiliano@gmail.com>
2021-11-17 14:34:39 +0100

    StarterClient primerCommit (no compila)

---
+++ src/main/java/com/cliente/StarterClienteApplication.java
@@ -0,0 +1,13 @@
+package com.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/controller/StarterClienteApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+
+class StarterClienteApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
